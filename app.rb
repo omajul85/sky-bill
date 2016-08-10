81187dca7e14c801e3a7bb9c6b6c182102adce12
Omar Alvarez <omajul2@hotmail.com>
2016-08-10 19:12:34 +0100

    Update partials with 2 decimals

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'sinatra/base'
 require 'sinatra/partial'
 require './lib/bill.rb'
+require './views/helpers/view_helpers.rb'
 
 class SkyBill < Sinatra::Base
+
+  include ViewHelpers
 
   set :public_folder, File.dirname(__FILE__) + '/public'
   
--- spec/features/bill_feature_spec.rb
+++ spec/features/bill_feature_spec.rb
@@ -13,26 +13,49 @@ feature 'Sky bill features:' do
       visit '/'
     end
   
-    it 'Prints the total due' do
-      expect(current_path).to eq '/'
-      expect(page).to have_content bill['total']
+    context 'Statement:' do
+      it 'Prints the total due' do
+        expect(current_path).to eq '/'
+        expect(page).to have_content bill['total']
+      end
+
+      it 'Prints the billing period' do
+        expect(current_path).to eq '/'
+        str = "From #{bill['statement']['period']['from']} to #{bill['statement']['period']['to']}"
+        expect(page).to have_content str
+      end
     end
 
-    it 'Prints the billing period' do
-      expect(current_path).to eq '/'
-      str = "From #{bill['statement']['period']['from']} to #{bill['statement']['period']['to']}"
-      expect(page).to have_content str
+    context 'My subscriptions:' do
+      it 'Prints the information about the subscriptions' do
+        expect(current_path).to eq '/'
+        subs = bill['package']['subscriptions'].first
+        expect(page).to have_content subs['type'].capitalize
+        expect(page).to have_content subs['name']
+        expect(page).to have_content subs['cost']
+      end
     end
 
-    it 'Does not print the list of calls (hidden by default)' do
-      expect(current_path).to eq '/'
-      find(:css, "#charges").should_not be_visible
+    context 'Call charges:' do
+      it 'Prints the information about the calls' do
+        expect(current_path).to eq '/'
+        call = bill['callCharges']['calls'].first
+        expect(page).to have_content call['called']
+        expect(page).to have_content call['duration']
+        expect(page).to have_content call['cost']
+      end
     end
 
-    it 'Prints the list of calls if button clicked' do
-      expect(current_path).to eq '/'
-      click_button('charges')
-      expect(page).to have_content "List of calls"
+    context 'Sky store:' do
+      it 'Prints the information about the store' do
+        expect(current_path).to eq '/'
+        rental = bill['skyStore']['rentals'].first
+        bk = bill['skyStore']['buyAndKeep'].first
+        expect(page).to have_content rental['title']
+        expect(page).to have_content rental['cost']
+        expect(page).to have_content bk['title']
+        expect(page).to have_content bk['cost']
+      end
     end
   end
 end
