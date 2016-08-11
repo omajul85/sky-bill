c774c8490fc67ba3cdecf12595844cd0a43a8769
Omar Alvarez <omajul2@hotmail.com>
2016-08-11 09:32:20 +0100

    Update Bill class with the url as initialization parameter

--- lib/bill.rb
+++ lib/bill.rb
@@ -2,15 +2,13 @@ require 'ostruct'
 require 'json'
 require 'http'
 
-# Send API request and converts json to an object via OpenStruct
+# Send API request and converts json to objects via OpenStruct
 class Bill
 	
-	URL = "https://safe-plains-5453.herokuapp.com/bill.json".freeze
-
 	attr_reader :statement, :total, :package, :callCharges, :skyStore
 
-	def initialize
-		data					= JSON.parse(HTTP.get(URL), object_class: OpenStruct)
+	def initialize(bill_url = "https://safe-plains-5453.herokuapp.com/bill.json")
+		data					= JSON.parse(HTTP.get(bill_url), object_class: OpenStruct)
 		@statement		= data.statement
 		@total				= data.total
 		@package			= data.package
