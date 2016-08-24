087e332677c59fc35607a9ad0e8ce4c6c7e03605
Omar Alvarez <omajul2@hotmail.com>
2016-08-24 11:31:40 +0100

    Fix indentation

--- lib/bill.rb
+++ lib/bill.rb
@@ -8,12 +8,12 @@ class Bill
 	attr_reader :statement, :total, :package, :callCharges, :skyStore
 
 	def initialize(bill_url = "https://safe-plains-5453.herokuapp.com/bill.json")
-		data					= JSON.parse(HTTP.get(bill_url), object_class: OpenStruct)
-		@statement		= data.statement
-		@total				= data.total
-		@package			= data.package
-		@callCharges	= data.callCharges
-		@skyStore			= data.skyStore
+		data = JSON.parse(HTTP.get(bill_url), object_class: OpenStruct)
+		@statement = data.statement
+		@total = data.total
+		@package = data.package
+		@callCharges = data.callCharges
+		@skyStore = data.skyStore
 	end
 
 end
